da351c65e6281c4ddec8ec48ea9f2e4a2fb7620e
matharoo <matharoo89@gmail.com>
2020-10-29 23:21:04 -0700

    busytask function on /compute for load testing

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+
+function busytask(limit = 4999999999) {
+  let count = 1;
+  for (let i = 0; i < limit; i++) {
+    count += 1;
+  }
+  return count;
+}
  
 app.listen(3000, function () {
   console.log("listening on 3000");
@@ -19,3 +27,9 @@ app.get("/create", (req, res) => {
   res.send("User was created!!");
   console.log("CREATE user");
 });
+
+app.get("/compute", (req, res) => {
+  console.log("BUSY task initiated "+Date.now());
+    let x= busytask();
+    res.send("finished loop and sum is : "+x);
+  });
